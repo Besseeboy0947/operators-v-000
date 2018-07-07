7a9e46d0f3c55981bbe6872346efb2dad0338bdc
Ikaquan Bessee <ikaquanbessee@yahoo.com>
2018-07-07 21:16:27 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -13,23 +13,6 @@ speed > 60 ? true : false
 speed > 60 ? true : false
  
  end
- 
- def unsafe?(speed)
-  speed = 50
- 
-speed > 60 ? true : false
- 
- end
- 
- 
- 
- def not_safe?(speed)
-  speed = 50
- 
-speed > 60 ? true : false
- 
- end
- 
 
  
  
